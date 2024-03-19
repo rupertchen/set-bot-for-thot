c84e6e061dbb92fa7f566342c56a086ee03884ff
Rupert Chen <rupert.chen@gmail.com>
2024-03-19 00:18:38 -0700

    Bulk deletes!
    This seems to be able to delete up to about 50 messages at a time.  We
    may still need to do iterations until there is nothing else to delete.

--- commands/utility/poke.js
+++ commands/utility/poke.js
@@ -16,34 +16,20 @@ module.exports = {
 
 		    // Filter messages
 		    const horizon = Date.now() - (1 * 60 * 1000);
-		    const filter = m => {
-			    console.log(`created at ${new Date(m.createdTimestamp)}, filter message ${m.content}`);
+		    const isTooOld = m => {
 			    return m.createdTimestamp < horizon;
 		    };
 
 		    // Extract IDs
-		    const msgIds = channel.messages.fetch()
-		        .then(messages => messages.filter(filter))
-		    	.then(messages => {
-			    console.log(`Received ${messages.size} messages`);
-			    channel.send(`DEBUG: fetched ${messages.size} messages`);
-			    
-			    messages.forEach((val, key, map) => {
-				    console.log(`m[${key}] = createdTimestamp:${val.createdTimestamp}, deletable:${val.deletable}`);
+		    const oldMessages = channel.messages.fetch()
+		        .then(messages => messages.filter(isTooOld))
+		        .then(messages => {
+			    const report = messages.map(x => {
+				    return {id: x.id, ts: x.createdTimestamp};
 			    });
-				console.log('message IDs', messages.keys());
-			    return messages.keys();
-			})
-		    	.catch(console.error);
-
-		    msgIds.then(ids => {
-			    console.log(ids);
-			    for (const id of ids) {
-				    console.log(`Delete message ${id}`);
-				    channel.messages.delete(id);
-			    }
-		    });
-
+			    console.log('Deleting messages:', report);
+			    channel.bulkDelete(messages);
+		    })
 	    })
 	    .catch(console.error);
 	await interaction.reply(`Searching for oldest message in ${interaction.channelId}`);
